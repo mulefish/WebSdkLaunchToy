60bea584c3bca96f15678584d8efc4532a3bcf0c
Paul Montgomery <48227329+mulefish@users.noreply.github.com>
2023-06-29 09:19:47 +0900

    Working it

--- src/common.js
+++ src/common.js
@@ -1,28 +1,35 @@
+const statuses = {
+  LOADING: "loading",
+  IDLE: "idle",
+  LOAD: "load",
+  READY: "ready"
+}
+
 function verdict(a, b, msg) {
-    if (JSON.stringify(a) === JSON.stringify(b)) {
-      console.log('PASS ' + msg);
-      return true;
-    } else {
-      console.log('FAIL ' + msg);
-      return false;
-    }
+  if (JSON.stringify(a) === JSON.stringify(b)) {
+    console.log('PASS ' + msg);
+    return true;
+  } else {
+    console.log('FAIL ' + msg);
+    return false;
   }
-  
-  function blue(msg) {
-    console.log(`%c ${msg}`, 'background:lightblue');
-  }
-  function pink(msg) {
-    console.log(`%c ${msg}`, 'background:pink');
-  }
-  
-  function green(msg) {
-    console.log(`%c ${msg}`, 'background:lightgreen');
-  }
-  
-  module.exports = {
-    verdict,
-    blue,
-    green,
-    pink,
-  };
-  
+}
+
+function blue(msg) {
+  console.log(`%c ${msg}`, 'background:lightblue');
+}
+function pink(msg) {
+  console.log(`%c ${msg}`, 'background:pink');
+}
+
+function green(msg) {
+  console.log(`%c ${msg}`, 'background:lightgreen');
+}
+
+module.exports = {
+  verdict,
+  blue,
+  green,
+  pink,
+  statuses
+};
--- src/tdd.js
+++ src/tdd.js
@@ -1,7 +1,26 @@
-// Nothing to test here...   ...This tdd is for javascript
+// Nothing really to test here...   ...This tdd is for javascript
 // For JSX stuff I will use a different ( outside ) mechanism for tdd
 // 
-const { verdict } = require('./common.js');
-
+const { verdict, statuses } = require('./common.js');
+function statusesTest() { 
+    const expected = [
+        'LOADING',
+        'IDLE',
+        'LOAD',
+        'READY'
+    ]
+    let isOk = true 
+    expected.forEach((key)=> {
+        if ( ! statuses.hasOwnProperty(key)) {
+            isOk = false 
+        }
+    })
+    const n = Object.keys( statuses).length 
+    if ( n !== expected.length ) {
+        isOk = false 
+    }
+    verdict(isOk, true, "statusesTest" )
+}
 if (!module.parent) {
+    statusesTest() 
 }
