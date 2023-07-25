7d9dc708129a40de62a5bcebda73a5ebe4ae7630
mulefish <48227329+mulefish@users.noreply.github.com>
2023-07-25 09:22:01 -0700

    1 file modified

--- src/links.jsx
+++ src/links.jsx
@@ -3,26 +3,35 @@ import * as React from 'react';
 function Links() {
   return (
     <span>
-      <a href="https://stackblitz.com/edit/stackblitz-starters-f3pua2?file=README.md,src%2Flinks.jsx,src%2FApp.js">
+      <a
+        target="_blank"
+        href="https://stackblitz.com/edit/stackblitz-starters-f3pua2?file=README.md,src%2Flinks.jsx,src%2FApp.js"
+      >
         stackblizt
       </a>
       &nbsp;{' '}
-      <a href="https://github.com/mulefish/WebSdkLaunchToy" target="_blank">
+      <a
+        target="_blank"
+        href="https://github.com/mulefish/WebSdkLaunchToy"
+        target="_blank"
+      >
         WebSdkLaunchToy
       </a>
       &nbsp;{' '}
-      <a href="https://lululemon.atlassian.net/wiki/spaces/DCP/pages/3235746045/Understand+Document+how+Adobe+Edge+Works+-+DATASE+394">
-        confluence
+      <a target="_blank" href="https://github.com/adobe/alloy">
+        Adobe Alloy ( npm run build - npm run dev )
       </a>
       &nbsp;{' '}
-      <a href="https://github.com/adobe/alloy">
-        Adobe Alloy ( Note: npm run build - npm run dev )
-      </a>
-      &nbsp;{' '}
-      <a href="https://lululemon.atlassian.net/wiki/spaces/DCP/pages/3248884892/Setup+Mechanism+to+Read+LAM+and+send+to+WebSDK+in+Launch+-+DATASE-379+and+426">
+      <a
+        target="_blank"
+        href="https://lululemon.atlassian.net/wiki/spaces/DCP/pages/3248884892/Setup+Mechanism+to+Read+LAM+and+send+to+WebSDK+in+Launch+-+DATASE-379+and+426"
+      >
         confluence ( pictures)
       </a>
-      <a href="https://lululemon.atlassian.net/wiki/spaces/DCP/pages/3235746045/Understand+Document+how+Adobe+Edge+Works+-+DATASE+394">
+      <a
+        target="_blank"
+        href="https://lululemon.atlassian.net/wiki/spaces/DCP/pages/3235746045/Understand+Document+how+Adobe+Edge+Works+-+DATASE+394"
+      >
         confluence article 2
       </a>
     </span>
